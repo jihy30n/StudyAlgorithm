9953f81105f43db6005c7e84a1c8f94a937380a2
jihy30n <jihyeon546@gmail.com>
2024-01-08 19:14:50 +0900

    알고리즘공부

---
+++ src/BaekJoon/day1.java
@@ -0,0 +1,10 @@
+package BaekJoon;
+
+public class day1 {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.print("Hasdfasdfasdfasd");
+    }
+
+}
